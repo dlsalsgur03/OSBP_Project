761e62a9bb5a637a3bd6a972715d3e725b80a929
skinheadman <zzzoad929@gmail.com>
2025-05-12 17:10:20 +0900

    Add MongoDB save function

--- lib/reservation/getFlask.dart
+++ lib/reservation/getFlask.dart
@@ -17,3 +17,40 @@ String getFlaskBaseUrl() {
   }
 }
 
+// 입력된 문자열 MongoDB에 저장
+Future<void> saveTextToMongoDB({
+      required String title,
+      required String location,
+      required String firstdate,
+      required String lastdate,
+      required String emoji,
+    }) async {
+  final newSchedule = {
+    'title' : title,
+    'place' : location,
+    'startdate' : firstdate,
+    'enddate' : lastdate,
+    'emoji' : emoji,
+  };
+  if (newSchedule.isEmpty) {
+    print("저장할 내용이 없습니다.");
+    return;
+  }
+
+  final String baseUrl = getFlaskBaseUrl();
+  final url = Uri.parse('$baseUrl/save-text'); // Flask API 엔드포인트
+
+  try {
+    final response = await http.post(
+      url,
+      headers: {
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(newSchedule),
+    );
+  } catch (e) {
+    print('MongoDB 저장 중 네트워크 또는 기타 오류: $e');
+
+    throw Exception('Error saving text: $e');
+  }
+}
