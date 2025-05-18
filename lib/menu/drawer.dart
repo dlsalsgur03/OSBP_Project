76b3fc95e50d87458d0eff46fea6090b3517a654
dlsalsgur03 <dlsalsgur2@gmail.com>
2025-05-18 14:14:51 +0900

    Add drawer insets & delete menuPopup class

--- lib/menu/drawer.dart
+++ lib/menu/drawer.dart
@@ -11,15 +11,35 @@ class _MenuDrawerState extends State<MenuDrawer> {
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      width: MediaQuery.of(context).size.width * 0.7,
       child: ListView(
         children: [
-          DrawerHeader(
-              child: Text("Drawer Header")
-          ),
           ListTile(
             leading: Icon(Icons.settings),
-            title: Text("Settings"),
+            hoverColor: Color(0xffdee2e6),
+            title: Text("설정"),
+            onTap: () {},
+            trailing: Icon(Icons.navigate_next),
+          ),
+          ListTile(
+            leading: Icon(Icons.people),
+            hoverColor: Color(0xffdee2e6),
+            title: Text("만든 사람들"),
+            onTap: () {},
+            trailing: Icon(Icons.navigate_next),
+          ),
+          ListTile(
+            leading: Icon(Icons.report),
+            hoverColor: Color(0xffdee2e6),
+            title: Text("오류 신고"),
+            onTap: () {},
+            trailing: Icon(Icons.navigate_next),
+          ),
+          ListTile(
+            leading: Icon(Icons.newspaper),
+            hoverColor: Color(0xffdee2e6),
+            title: Text("공지사항"),
+            onTap: () {},
+            trailing: Icon(Icons.navigate_next),
           ),
         ],
       ),
