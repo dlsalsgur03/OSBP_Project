c8bb8b789430edb487f8241bb8477d9f6bfefe69
yuntae <jungryeok168@naver.com>
2025-05-28 00:47:58 +0900

    병합하면서 삭제된 코드 복구

--- lib/calendar/calendar.dart
+++ lib/calendar/calendar.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
 
 import '../weather/weather.dart';
-
+import '../reservation/reading_json.dart';
 class Calendar extends StatefulWidget {
   const Calendar({super.key});
 
@@ -81,6 +81,20 @@ class _CalendarState extends State<Calendar> {
               );
           }
           return const Center();
+        },
+        markerBuilder: (context, date, events) {
+          final normalizedDate = DateTime(date.year, date.month, date.day);
+          if (scheduledDates.contains(normalizedDate)) {
+            return Positioned(
+              bottom: 1,
+              child: Container(
+                width: 30,
+                height: 2,
+                color: Color(0xffa7385c), // 밑줄 색상
+              ),
+            );
+          }
+          return null;
         },
       ),
       headerStyle: HeaderStyle(
