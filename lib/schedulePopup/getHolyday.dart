4dfbbc472e681b6477fa7fa14403ba4d4c04f1ec
skinheadman <zzzoad929@gmail.com>
2025-06-02 16:01:17 +0900

    1년단위로 받도록 변경

--- lib/schedulePopup/getHolyday.dart
+++ lib/schedulePopup/getHolyday.dart
@@ -3,18 +3,35 @@ import 'dart:convert';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 Future<List<DateTime>> fetchHolidays(int year) async {
-  final HolidayKey = dotenv.env['HOLIDAY_API_KEY'];
-  final url = Uri.parse('http://apis.data.go.kr/B090041/openapi/service/SpcdeInfoService/getHoliDeInfo?solYear=$year&ServiceKey=$HolidayKey&_type=json');
+  final apiKey = dotenv.env['HOLIDAY_API_KEY'];
+  final List<DateTime> allHolidays = [];
 
-  final response = await http.get(url);
-  if (response.statusCode == 200) {
-    final data = json.decode(response.body);
-    final items = data['response']['body']['items']['item'] as List<dynamic>;
-    return items.map((item) {
-      final dateStr = item['locdate'].toString(); // e.g. "20250505"
-      return DateTime.parse(dateStr);
-    }).toList();
-  } else {
-    throw Exception('공휴일 데이터를 불러오는 데 실패했습니다');
+  for (int month = 1; month <= 12; month++) {
+    final url = Uri.parse(
+        'http://apis.data.go.kr/B090041/openapi/service/SpcdeInfoService/getHoliDeInfo?solYear=$year&solMonth=${month.toString().padLeft(2, '0')}&ServiceKey=$apiKey&_type=json');
+
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      final items = data['response']['body']['items']['item'];
+
+      if (items != null) {
+        // items가 List일 수도, Map일 수도 있음
+        if (items is List) {
+          allHolidays.addAll(items.map((item) {
+            final dateStr = item['locdate'].toString(); // e.g. "20250505"
+            return DateTime.parse(dateStr);
+          }));
+        } else if (items is Map) {
+          final dateStr = items['locdate'].toString();
+          allHolidays.add(DateTime.parse(dateStr));
+        }
+      }
+    } else {
+      throw Exception('[$month월] 공휴일 데이터를 불러오는 데 실패했습니다');
+    }
   }
+
+  return allHolidays;
 }
+
