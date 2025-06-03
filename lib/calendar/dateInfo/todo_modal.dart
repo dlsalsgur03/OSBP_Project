ea9be06986a1f1a14e79bdebd89f581b151b922c
dlsalsgur03 <dlsalsgur2@gmail.com>
2025-06-03 13:09:31 +0900

    shared preference를 이용하기 위한 todo modal 변경

--- lib/calendar/dateInfo/todo_modal.dart
+++ lib/calendar/dateInfo/todo_modal.dart
@@ -1,5 +1,19 @@
 class Todo {
   final String task;
   bool done;
-  Todo({required this.task, this.done = false});
+
+  Todo({
+    required this.task,
+    this.done = false,
+  });
+
+  Map<String, dynamic> toJson() => {
+    'task': task,
+    'done': done,
+  };
+
+  factory Todo.fromJson(Map<String, dynamic> json) => Todo(
+    task: json['task'],
+    done: json['done'],
+  );
 }
