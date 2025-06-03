e88b394edd9688f3a926f42a5bf0674d66c42489
dlsalsgur03 <dlsalsgur2@gmail.com>
2025-06-03 13:41:52 +0900

    shared preference를 이용하기 위한 todo function 수정

--- lib/calendar/dateInfo/todo_func.dart
+++ lib/calendar/dateInfo/todo_func.dart
@@ -1,31 +1,47 @@
+import 'dart:convert';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'todo_modal.dart';
-import 'package:intl/intl.dart';
 
-final Map<String, List<Todo>> _todoStorage = {};
-
-String _formatDateKey(DateTime date) => DateFormat('yyyy-MM-dd').format(date);
-
-Future<List<Todo>> getTodoList(DateTime date) async {
-  final key = _formatDateKey(date);
-  await Future.delayed(Duration(milliseconds: 200));
-  return _todoStorage[key]?.toList() ?? [];
+String _formatDate(DateTime date) {
+  return '${date.year}-${date.month}-${date.day}';
 }
 
-void addTodoToStorage(DateTime date, Todo todo) {
-  final key = _formatDateKey(date);
-  _todoStorage[key] = (_todoStorage[key] ?? [])..add(todo);
+Future<void> saveTodos(DateTime date, List<Todo> todos) async {
+  final prefs = await SharedPreferences.getInstance();
+  final key = _formatDate(date);
+  final jsonString = jsonEncode(todos.map((todo) => todo.toJson()).toList());
+  await prefs.setString(key, jsonString);
 }
 
-void updateTodoInStorage(DateTime date, int index, bool done) {
-  final key = _formatDateKey(date);
-  if (_todoStorage.containsKey(key) && index < _todoStorage[key]!.length) {
-    _todoStorage[key]![index].done = done;
+Future<List<Todo>> loadTodos(DateTime date) async {
+  final prefs = await SharedPreferences.getInstance();
+  final key = _formatDate(date);
+  final jsonString = prefs.getString(key);
+
+  if (jsonString == null) return [];
+
+  final List<dynamic> jsonList = jsonDecode(jsonString);
+  return jsonList.map((json) => Todo.fromJson(json)).toList();
+}
+
+Future<void> addTodo(DateTime date, Todo todo) async {
+  final todos = await loadTodos(date);
+  todos.add(todo);
+  await saveTodos(date, todos);
+}
+
+Future<void> removeTodo(DateTime date, int index) async {
+  final todos = await loadTodos(date);
+  if (index >= 0 && index < todos.length) {
+    todos.removeAt(index);
+    await saveTodos(date, todos);
   }
 }
 
-void removeTodoFromStorage(DateTime date, int index) {
-  final key = _formatDateKey(date);
-  if (_todoStorage.containsKey(key) && index < _todoStorage[key]!.length) {
-    _todoStorage[key]!.removeAt(index);
+Future<void> updateTodo(DateTime date, int index, bool done) async {
+  final todos = await loadTodos(date);
+  if (index >= 0 && index < todos.length) {
+    todos[index].done = done;
+    await saveTodos(date, todos);
   }
 }
