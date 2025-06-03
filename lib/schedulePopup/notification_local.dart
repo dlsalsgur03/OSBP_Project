1cd289827f67377047eee65ee8022e402cbabb19
skinheadman <zzzoad929@gmail.com>
2025-06-03 19:18:21 +0900

    Erase the permission function and Add getRegion function

--- lib/schedulePopup/notification_local.dart
+++ lib/schedulePopup/notification_local.dart
@@ -1,12 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:geolocator/geolocator.dart';
-import 'package:permission_handler/permission_handler.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-Future<Position> getCurrentLocation() async {
-  var status = await Permission.location.request();
-  if(status.isGranted) {
-    return await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+Future<String> getRegion(Position pos) async {
+  var apiKey = dotenv.env['KAKAO_API_KEY'];
+  final url = Uri.parse('https://dapi.kakao.com/v2/local/geo/coord2regioncode.json?x=${pos.longitude}&y=${pos.latitude}');
+
+  final res = await http.get(url, headers: {
+    'Authorization': 'KakaoAK $apiKey',
+  });
+
+  if(res.statusCode == 200) {
+    final data = jsonDecode(res.body);
+    return data['documents'][0]['region_3depth_name']; // 지역명(동이름) 받아오기
   } else {
-    throw Exception("Location permission denied");
+    throw Exception('Failed to get region');
   }
 }
