431163faf7e8ccc5baeb6918b7b6368082acd0db
skinheadman <zzzoad929@gmail.com>
2025-06-04 10:34:25 +0900

    Add Sorting Recommended transporation function by distance

--- lib/reservation/transportation_recommend.dart
+++ lib/reservation/transportation_recommend.dart
@@ -2,13 +2,23 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-const int searchRadius = 10000; // 10km 반경
+const int searchRadius = 50000; // 50km 반경
+
+Future<Map<String, dynamic>> findNearestStation(double x, double y) async {
+  final busResults = await searchKakaoPlaces('버스 터미널', x, y);
+  final trainResults = await searchKakaoPlaces('기차역', x, y);
+
+  // 두 리스트를 합치고 거리 기준으로 정렬
+  final allPlaces = [...busResults, ...trainResults];
+  allPlaces.sort((a, b) => int.parse(a['distance']).compareTo(int.parse(b['distance'])));
+
+  // 가장 가까운 결과 리턴
+  return allPlaces.isNotEmpty ? allPlaces.first : {};
+}
 
 Future<List<Map<String, dynamic>>> searchKakaoPlaces(String query, double x, double y) async {
   var apiKey = dotenv.env['KAKAO_API_KEY'];
-  final url = Uri.parse(
-      'https://dapi.kakao.com/v2/local/search/keyword.json'
-          '?query=$query&x=$x&y=$y&radius=$searchRadius&sort=distance'
+  final url = Uri.parse('https://dapi.kakao.com/v2/local/search/keyword.json?query=$query&x=$x&y=$y&radius=$searchRadius&sort=distance'
   );
 
   final response = await http.get(url, headers: {
